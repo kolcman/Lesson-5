c6ceb225691cb2eb9d45289c1fc83625064813ae
Kolcman <m.kolcman@yandex.ru>
2024-02-14 13:45:25 +0300

    Lesson5:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿void ZeroEvenElements(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array[i] = 0;
+        }
+    }
+}
+
+void printArray(int[] array)
+{
+    foreach (int e in array)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+
+int[] array = { 5, 3, 5, 7, 82, 2, 6 };
+ZeroEvenElements(array);
+printArray(array);
