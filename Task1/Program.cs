a19aa66a567cd2db3c93cf84586a78be5fa27700
Kolcman <m.kolcman@yandex.ru>
2024-02-14 13:03:30 +0300

    First commit

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    // return ((a + b) / (c + d));
+    double numenator = a + b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+// System.Console.WriteLine(CalculateFormula(76, 4, 7, 6));
+System.Console.WriteLine(CalculateFormula(3, 5, 7, 4));
